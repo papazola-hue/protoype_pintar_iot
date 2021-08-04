0b20b01371333fab346a2ccf551a7447636c8f9c
Phicos Dev <phicosdev@gmail.com>
2021-08-04 14:55:43 +0700

    chart UI

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -100,9 +100,157 @@ class _HomeScreenState extends State<HomeScreen> {
                           fontWeight: FontWeight.w400,
                         ),
                       ],
-                    ))
+                    )),
               ],
-            )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TemplateTextWidget(
+              title: "Statistik Alat 1",
+              size: 24,
+              fontWeight: FontWeight.bold,
+              warna: Colors.grey,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            FutureBuilder(
+              future: dbRef.once(),
+              builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                // try {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container(
+                      child: Center(child: CircularProgressIndicator()));
+                }
+                if (snapshot.hasError) {
+                  return Container(
+                      child: Center(
+                          child: TemplateTextWidget(
+                            title: "Terjadi Kesalahan",
+                          )));
+                }
+                if (snapshot.hasData) {
+                  lists.clear();
+                  data1.clear();
+                  Map<dynamic, dynamic> values = snapshot.data.value;
+                  values.forEach((key, values) {
+                    // lists.add(values);
+                    if (values['Id'] == "Alat 01") {
+                      data1.add(DataStatistik(values['Waktu'].toString(),
+                          values["Kelembapan"].toString(), Colors.red));
+                    }
+                  });
+                  var series = [
+                    charts.Series(
+                      domainFn: (DataStatistik dataStatistik, _) =>
+                      dataStatistik.title,
+                      measureFn: (DataStatistik dataStatistik, _) =>
+                          int.parse(dataStatistik.kelembapan),
+                      colorFn: (DataStatistik dataStatistik, _) =>
+                      dataStatistik.color,
+                      id: 'Clicks',
+                      data: data1,
+                    ),
+                  ];
+                  var chart = charts.BarChart(
+                    series,
+                    animate: true,
+                  );
+
+                  var chartWidget = Padding(
+                    padding: EdgeInsets.all(32.0),
+                    child: SizedBox(
+                      height: 200.0,
+                      child: chart,
+                    ),
+                  );
+                  return Container(
+                    child: chartWidget,
+                  );
+                } else {
+                  return Container(
+                      child: Center(
+                          child: TemplateTextWidget(
+                            title: "Data Kosong",
+                          )));
+                }
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TemplateTextWidget(
+              title: "Statistik Alat 2",
+              size: 24,
+              fontWeight: FontWeight.bold,
+              warna: Colors.grey,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            FutureBuilder(
+              future: dbRef.once(),
+              builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                // try {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container(
+                      child: Center(child: CircularProgressIndicator()));
+                }
+                if (snapshot.hasError) {
+                  return Container(
+                      child: Center(
+                          child: TemplateTextWidget(
+                            title: "Terjadi Kesalahan",
+                          )));
+                }
+                if (snapshot.hasData) {
+                  lists.clear();
+                  data2.clear();
+                  Map<dynamic, dynamic> values = snapshot.data.value;
+                  values.forEach((key, values) {
+                    // lists.add(values);
+                    if (values['Id'] == "Alat 02") {
+                      data2.add(DataStatistik(values['Waktu'].toString(),
+                          values["Kelembapan"].toString(), Colors.red));
+                    }
+                  });
+                  var series = [
+                    charts.Series(
+                      domainFn: (DataStatistik dataStatistik, _) =>
+                      dataStatistik.title,
+                      measureFn: (DataStatistik dataStatistik, _) =>
+                          int.parse(dataStatistik.kelembapan),
+                      colorFn: (DataStatistik dataStatistik, _) =>
+                      dataStatistik.color,
+                      id: 'Clicks',
+                      data: data2,
+                    ),
+                  ];
+                  var chart = charts.BarChart(
+                    series,
+                    animate: true,
+                  );
+
+                  var chartWidget = Padding(
+                    padding: EdgeInsets.all(32.0),
+                    child: SizedBox(
+                      height: 200.0,
+                      child: chart,
+                    ),
+                  );
+                  return Container(
+                    child: chartWidget,
+                  );
+                } else {
+                  return Container(
+                      child: Center(
+                          child: TemplateTextWidget(
+                            title: "Data Kosong",
+                          )));
+                }
+              },
+            ),
             // TemplateCardWidget(
             //   title: 'History',
             //   widget: Icon(FontAwesomeIcons.history),
